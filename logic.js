ffadfe6454d071019668777e1a1a5b1b3e006095
Rachel Powell <32336001+rvp0005@users.noreply.github.com>
2019-03-24 15:37:36 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,45 +1,50 @@
-// Creating map object
+var queryUrl = "http://data.insideairbnb.com/united-states/la/new-orleans/2019-03-06/visualisations/neighbourhoods.geojson"
+
+d3.json(queryUrl, function(data) {
+  createFeatures(data.features);
+});
+
+function createFeatures(airbnbData) {
+
+// makes the borough outlines
+    //need to edit to make borough name pop up; current reads undefined. 
+  function onEachFeature (feature, layer) {
+     layer.bindPopup("<h3>" + feature[0] +
+      "</h3>");
+  }
+    var neighborhoods = L.geoJSON(airbnbData, {
+    onEachFeature: onEachFeature
+  });
+
+  createMap(neighborhoods)
+}
+
+function createMap (neighborhoods) {
+
+// Adding tile layer to the map
+var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+})
+
+var baseMaps = {
+  "Light Map": lightmap
+};
+
+var overlayMaps = {
+"Neighborhood Boundaries": neighborhoods
+ };
+
 var myMap = L.map("map", {
-    center: [29.9511, -90.0715],
-    zoom: 11
+    center: [29.95, -90.0715],
+    zoom: 5,
+    layers: [lightmap, neighborhoods]
   });
-  
-  // Adding tile layer to the map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
   }).addTo(myMap);
-  
-  // TODO:ps://data.cityofchicago.org/resource/6zsd-86xi.json
-  // ?$where=date between '2015-01-10T12:00:00' and '2015-01-10T14:00:00'
-  
-  // Store API query variables
-  var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-  // Add the dates in the ISO formats
-  var date = "$where=created_date between '2015-01-01T12:00:00' and '2016-01-15T14:00:00'";
-  // Add the complaint type
-  var complaint = "&complaint_type=Rodent";
-  // Add a limit
-  var limit = "&$limit=1000000";
-  
-  var url = baseURL+date+complaint+limit
-  // Assemble API query URL
-  
-  // Grab the data with d3
-  d3.json(url, function(response) {
-    console.log(response)
-  
-    var markers = L.markerClusterGroup();
-  
-    for (var i = 0; i < response.length; i++) {
-      var location = response[i].location;
-      if (location) {
-        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-          .bindPopup(response[i].descriptor));
-      }
-    }
-  
-    myMap.addLayer(markers);
-  });
+
+};
