3014224ec4b61286afc9caa17218120120864093
unknown <lorieharris@gmail.com>
2019-03-26 21:40:24 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,40 @@
-function buildMetadata(sample) {
-  d3.json(`/listings/${sample}`, function(data) {
-    var data = [data];
-    console.log(data);
-  var meta_chart = d3.select("#sample-metadata");
-  meta_chart.html("");
-  data.forEach((data) => {
-    var row = meta_chart.append("tbody");
-    Object.entries(data).forEach(([key, value]) => {
-      var cell = row.append("tr");
+// function buildMetadata(sample) {
+//   d3.json(`/listings/${sample}`, function(data) {
+//     var data = [data];
+//     console.log(data);
+//   var meta_chart = d3.select("#sample-metadata");
+//   meta_chart.html("");
+//   data.forEach((data) => {
+//     var row = meta_chart.append("tbody");
+//     Object.entries(data).forEach(([key, value]) => {
+//       var cell = row.append("tr");
       
-      cell.text(`${key}: ${value}`);
+//       cell.text(`${key}: ${value}`);
       
-    console.log(`Key: ${key} | Value: ${value}`);
-    });
-  });
-});
-}
-function init () {
-  var selector = d3.select("#selDataset");
+//     console.log(`Key: ${key} | Value: ${value}`);
+//     });
+//   });
+// });
+// }
+// function init () {
+//   var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names", function(sampleNames) {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
+//   // Use the list of sample names to populate the select options
+//   d3.json("/names", function(sampleNames) {
+//     sampleNames.forEach((sample) => {
+//       selector
+//         .append("option")
+//         .text(sample)
         
-    });
-  });
-}
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  // buildCharts(newSample);
-  buildMetadata(newSample);
-}
+//     });
+//   });
+// }
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   // buildCharts(newSample);
+//   buildMetadata(newSample);
+// }
 
-// Initialize the dashboard
-init();
+// // Initialize the dashboard
+// init();
 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,45 @@
+
+function buildMetadata(sample) {
+  
+  d3.json(`/listings/${sample}`, function(data) {
+    var data = [data];
+    console.log(data);
+  var meta_chart = d3.select("#sample-metadata");
+  meta_chart.html("");
+  data.forEach((data) => {
+    var row = meta_chart.append("tbody");
+    Object.entries(data).forEach(([key, value]) => {
+      var cell = row.append("tr");
+      
+      cell.text(`${key}: ${value}`);
+      
+    console.log(`Key: ${key} | Value: ${value}`);
+    });
+  });
+});
+}
+function init () {
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names", function(sampleNames) {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        
+    });
+  });
+}
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  // buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
+
 var url = "/static/neighbourhoods.geojson";
 
 var neighborhood_list = [];
@@ -90,22 +132,4 @@ L.control.layers(basemaps, overlaymaps, {
   
 
 
-// function init() {
-  // Grab a reference to the dropdown select element
 
-
-  //   // Use the first sample from the list to build the initial plots
-  //   const firstSample = sampleNames[0];
-  //   buildCharts(firstSample);
-  //   buildMetadata(firstSample);
-  // });
-// }
-
-// function optionChanged(newSample) {
-//   // Fetch new data each time a new sample is selected
-//   buildCharts(newSample);
-//   buildMetadata(newSample);
-// }
-
-// Initialize the dashboard
-// init();
