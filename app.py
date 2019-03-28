3127455f1bf1e6352d1245f1a4d8f3dba4fec70b
User <lorieharris@gmail.com>
2019-03-28 14:50:16 -0400

    update

--- app.py
+++ app.py
@@ -7,22 +7,16 @@ import os
 from bson.objectid import ObjectId
 import pprint
 import sys
+import json
 
 
-
-# app = Flask(__name__)
 app = Flask(__name__, static_url_path='', static_folder="")
-# setup mongo connection
+
 
 mongo = PyMongo(app, uri="mongodb://Lori:Les4783!@ds223756.mlab.com:23756/heroku_r58qkhd7")
 
-# connect to mongo db and collection
-
-
-
 @app.route("/")
 def index():
-    # write a statement that finds all the items in the db and sets it to a variable
     listings_info = mongo.db.listings.find_one()
     json_info = mongo.db.geojson.find_one({})
   
@@ -31,21 +25,54 @@ def index():
 
 @app.route("/names")
 def names():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    
+   
     collection = mongo.db.neighborhoods
     data = pd.DataFrame(list(collection.find({})))
-    # Return a list of the column names (sample names)
     return jsonify(list(data.neighbourhood))
+
+@app.route("/geojson")
+def geojson():
+    collection = mongo.db.geojson
+    data = {}
+    myquery = {}   
+
+    for json in collection.find(myquery):
+        data.update({'type': json['features']})
+    
+    return jsonify(data)
+
+@app.route("/bikeshare")
+def bikeshare():
+    collection = mongo.db.bikeshare_json
+    data = {}
+    myquery = {}   
+
+    for json in collection.find(myquery):
+        data.update({'type': json['features']})
+    
+    return jsonify(data)
+
+@app.route("/reviews_json")
+def reviews_json():
+    collection = mongo.db.reviews_json
+    data = {}
+    myquery = {}   
+
+    for json in collection.find(myquery):
+        data.update({'id': json['id'],
+        'listing_id' : json['listing_id'],
+        'date' : json['date'],
+        'reviewer_id': json['reviewer_id'],
+        'reviewer_name' :json['reviewer_name'],
+        'comments' : json['comments']      
+        })
+    
+    return jsonify(data)
 
 @app.route("/listings/<name>")
 def listings(name):
 
-    
-    # client = MongoClient()
-    # db = client["air_bnb"]
+  
     collection = mongo.db["listings"]
 
     data1 = {}
@@ -66,10 +93,13 @@ def listings(name):
         'Reviews_per_month':listing['reviews_per_month'],
         'Availability':listing['availability_365']
         })
-    print(data1)
+
     return jsonify(data1)
-   
 
 
+
+
+if __name__== '__main__':
+    app.run(debug=True)
 if __name__== '__main__':
     app.run(debug=True)
